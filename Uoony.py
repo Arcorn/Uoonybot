7afaea74237a20f3b7f2a565f2a661172ab15cb4
Arcorn <48410043+Arcorn@users.noreply.github.com>
2020-02-24 12:29:46 +0900

    Update Uoony.py

--- Uoony.py
+++ Uoony.py
@@ -102,5 +102,6 @@ async def on_message(message):
         await message.channel.send(foodresult)
 
 access_token = os.environ["BOT_TOKEN"]
+git_access_token = os.environ["GIT_TOKEN"]
 client.run(access_token)
 
